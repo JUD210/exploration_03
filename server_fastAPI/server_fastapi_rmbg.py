935e9cc0dd6d08b14cba9af8c593bc99e8f0d11b
rumelay <rumalay@naver.com>
2024-11-28 15:57:06 +0900

    add

--- server_fastAPI/server_fastapi_rmbg.py
+++ server_fastAPI/server_fastapi_rmbg.py
@@ -6,7 +6,7 @@ from io import BytesIO
 from transformers import AutoModelForImageSegmentation
 import torch
 from torchvision import transforms
-from PIL import Image
+from PIL import Image, ImageOps  # ImageOps 추가
 
 # 디바이스 설정 (GPU가 사용 가능하면 GPU 사용)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -28,8 +28,8 @@ def transform_image(image):
     transform = transforms.Compose(
         [
             transforms.Resize(image_size),  # 이미지 크기 조정
-            transforms.ToTensor(),  # 텐서로 변환
-            transforms.Normalize(  # 정규화
+            transforms.ToTensor(),          # 텐서로 변환
+            transforms.Normalize(           # 정규화
                 mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
             ),
         ]
@@ -65,7 +65,13 @@ app = FastAPI()
 async def remove_bg_endpoint(file: UploadFile = File(...)):
     # 업로드된 파일 읽기
     contents = await file.read()
-    image = Image.open(BytesIO(contents)).convert("RGB")
+    image = Image.open(BytesIO(contents))
+
+    # EXIF 회전 정보 적용
+    image = ImageOps.exif_transpose(image)
+
+    # 필요하면 RGB로 변환
+    image = image.convert("RGB")
 
     # 배경 제거 수행
     result_image = remove_background(image)
